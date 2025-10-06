bb93c6f7373f7a1640cdbd40712b88621f778a55
Matsuzaki Yuto <matsuzaki@MatsuzakinoMacBook-Air.local>
2025-10-06 15:46:09 +0900

    20251006 v2

--- lib/widgets/person_avatar.dart
+++ lib/widgets/person_avatar.dart
@@ -1,6 +1,4 @@
 import 'dart:io';
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
 import 'package:characters/characters.dart';
 
@@ -144,124 +142,33 @@ class _PresetPersonAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    switch (type) {
-      case _PresetAvatarType.mother:
-      case _PresetAvatarType.father:
-        break;
-    }
-
     final Color resolvedBackgroundColor =
-        backgroundColor ?? _AvatarPainter.kDefaultBackgroundColor;
+        backgroundColor ?? kPersonAvatarBackgroundColor;
 
-    final CustomPainter painter;
+    final String assetName;
     switch (type) {
       case _PresetAvatarType.mother:
+        assetName = 'assets/images/mother.png';
+        break;
       case _PresetAvatarType.father:
-        painter = _AvatarPainter(backgroundColor: resolvedBackgroundColor);
+        assetName = 'assets/images/father.png';
         break;
     }
 
     return SizedBox(
       width: size,
       height: size,
-      child: CustomPaint(
-        painter: painter,
+      child: ClipOval(
+        child: Container(
+          color: resolvedBackgroundColor,
+          child: Image.asset(
+            assetName,
+            width: size,
+            height: size,
+            fit: BoxFit.cover,
+          ),
+        ),
       ),
     );
-  }
-}
-
-class _AvatarPainter extends CustomPainter {
-  const _AvatarPainter({required this.backgroundColor});
-
-  final Color backgroundColor;
-
-  static const Color kDefaultBackgroundColor = Color(0xFFF7F7FA);
-  static const Color _faceColor = Color(0xFFFFE082);
-  static const Color _outlineColor = Color(0xFF333333);
-  static const Color _eyeColor = Color(0xFF000000);
-  static const Color _mouthColor = Color(0xFF000000);
-
-  static const double _faceDiameterFactor = 0.7;
-  static const double _faceStrokeFactor = 2 / 32;
-  static const double _eyeDiameterFactor = 0.12;
-  static const double _eyeOffsetXFactor = 0.16;
-  static const double _eyeOffsetYFactor = 0.08;
-  static const double _mouthWidthFactor = 0.36;
-  static const double _mouthHeightFactor = 0.22;
-  static const double _mouthOffsetYFactor = 0.16;
-  static const double _mouthStrokeFactor = 1.8 / 32;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final double dimension = math.min(size.width, size.height);
-    final Offset center = Offset(size.width / 2, size.height / 2);
-
-    final Paint backgroundPaint = Paint()
-      ..color = backgroundColor
-      ..style = PaintingStyle.fill
-      ..isAntiAlias = true;
-    canvas.drawCircle(center, dimension / 2, backgroundPaint);
-
-    final double faceDiameter = dimension * _faceDiameterFactor;
-    final double faceRadius = faceDiameter / 2;
-
-    final Paint facePaint = Paint()
-      ..color = _faceColor
-      ..style = PaintingStyle.fill
-      ..isAntiAlias = true;
-    canvas.drawCircle(center, faceRadius, facePaint);
-
-    final Paint outlinePaint = Paint()
-      ..color = _outlineColor
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = dimension * _faceStrokeFactor
-      ..strokeCap = StrokeCap.round
-      ..isAntiAlias = true;
-    canvas.drawCircle(center, faceRadius, outlinePaint);
-
-    final Paint featurePaint = Paint()
-      ..color = _eyeColor
-      ..style = PaintingStyle.fill
-      ..isAntiAlias = true;
-    final double eyeRadius = (dimension * _eyeDiameterFactor) / 2;
-    final double eyeOffsetX = dimension * _eyeOffsetXFactor;
-    final double eyeOffsetY = dimension * _eyeOffsetYFactor;
-
-    canvas.drawCircle(
-      center.translate(-eyeOffsetX, -eyeOffsetY),
-      eyeRadius,
-      featurePaint,
-    );
-    canvas.drawCircle(
-      center.translate(eyeOffsetX, -eyeOffsetY),
-      eyeRadius,
-      featurePaint,
-    );
-
-    final Rect mouthRect = Rect.fromCenter(
-      center: center.translate(0, dimension * _mouthOffsetYFactor),
-      width: dimension * _mouthWidthFactor,
-      height: dimension * _mouthHeightFactor,
-    );
-    final Paint mouthPaint = Paint()
-      ..color = _mouthColor
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = dimension * _mouthStrokeFactor
-      ..strokeCap = StrokeCap.round
-      ..isAntiAlias = true;
-
-    canvas.drawArc(
-      mouthRect,
-      math.pi * 0.15,
-      math.pi - (math.pi * 0.3),
-      false,
-      mouthPaint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant _AvatarPainter oldDelegate) {
-    return backgroundColor != oldDelegate.backgroundColor;
   }
 }
